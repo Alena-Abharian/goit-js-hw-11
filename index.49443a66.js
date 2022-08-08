9c71cfd263e92046b2fbe5eafdc61ae31da927e8
Alena-Abharian <94906025+Alena-Abharian@users.noreply.github.com>
2022-08-08 22:27:34 +0000

    Deploying to gh-pages from @ Alena-Abharian/goit-js-hw-11@c32c647fb5adf3618dd9d88a5c1b70e85df15224 🚀

---
+++ index.49443a66.js
@@ -0,0 +1,2 @@
+document.querySelector("button").addEventListener("click",(function(){console.log("Hi people")}));
+//# sourceMappingURL=index.49443a66.js.map
---
+++ index.827ba9d5.js
@@ -0,0 +1,2 @@
+document.querySelector("button").addEventListener("click",(function(){console.log("Hi people")}));
+//# sourceMappingURL=index.827ba9d5.js.map
